280f9809ea8ad749081168c92fe24c1dbe515e8c
blossun <dnsngu@gmail.com>
2021-04-24 22:53:26 +0900

    [Create 구현] 3. create form 구현
    onSubmit 이벤트 생성
    
    issue #2

--- react-app/src/components/CreateContent.js
+++ react-app/src/components/CreateContent.js
@@ -6,8 +6,20 @@ class CreateContent extends Component {
 		return (
 			<article>
 				<h2>Create</h2>
-				<form>
-					
+				<form action="/create_process" method="post"
+					onSubmit={function (e) {
+						e.preventDefault(); //페이지변경 동작을 off
+						alert('Submit!!!!!');
+					}.bind(this)}
+				>
+					<p><input type="text" name="title" placeholder="title"></input></p>
+					<p><input type="text" name="title" placeholder="title"></input></p>
+					<p>
+						<textarea name="desc" placeholder="description"></textarea>
+					</p>
+					<p>
+						<input type="submit"></input>
+					</p>
 				</form>
 			</article>
 		);
