ace2204230fccfbd62c06b7d1ecdac774003ecff
blossun <dnsngu@gmail.com>
2021-04-25 23:25:04 +0900

    [Delete 구현]
    Control 컴포넌트의 onChangeMode가 호출될 때, mode의 값이 `delete`이면 삭제를 진행
    삭제가 아니면 페이지 전환만 해준다.
    
    issue #3

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     super(props)
     this.max_content_id = 3;
     this.state = {
-      mode: 'create',
+      mode: 'welcome',
       selected_content_id: 2,
       subject: { title: 'WEB', sub: 'World Wide Web!' },
       welcome: { title: 'Welcome', desc: 'Hello, React!!!' },
@@ -105,9 +105,27 @@ class App extends Component {
           data={this.state.contents}
         ></TOC>
         <Control onChangeMode={function(_mode){
-          this.setState({
-            mode:_mode
-          });
+          if (_mode === 'delete') {
+            if (window.confirm('really?')) {
+              var _contents = Array.from(this.state.contents);
+              var i = 0;
+              while (i < _contents.length) {
+                if (_contents[i].id === this.state.selected_content_id) {
+                  _contents.splice(i,1); //배열의 i번째부터 1개 원소를 삭제
+                  break;
+                }
+                i++;
+              }
+              this.setState({
+                mode:'welcome',
+                contents:_contents
+              });
+            }
+          } else {
+            this.setState({
+              mode: _mode
+            });
+          }
         }.bind(this)}></Control>
         {this.getContent()}
       </div>
