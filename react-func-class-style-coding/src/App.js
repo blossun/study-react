365dee5a9306fdd5322f34b994052d1a21f198ba
blossun <dnsngu@gmail.com>
2021-04-26 12:25:50 +0900

    함수에서 state를 상용
    함수 방식에서 state를 만들 때는 useState()를 호출하고, 인자값으로 state의 초기값을 넣어준다.
    useState()는 배열을 리턴한다.
    배열의 0번 인덱스 값 : state의 상태값,
    배열의 1번 인덱스 값 : 그 상태를 바꿀 수 있는 함수
    
    issue #4

--- react-func-class-style-coding/src/App.js
+++ react-func-class-style-coding/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
@@ -12,10 +12,21 @@ function App() {
 }
 
 function FuncComp(props) {
+  var numberState = useState(props.initNumber);
+  var number = numberState[0]; //state값
+  var setNumber = numberState[1]; //함수가 담긴다. 이 함수를 통해서 number의 값을 변경할 수 있다.
+
+  console.log('numberState', numberState);
+
   return (
     <div className="container">
       <h2>function style component</h2>
-      <p>Number : {props.initNumber}</p>
+      <p>Number : {number}</p>
+      <input type="button" value="random" onClick={
+          function(){
+            setNumber(Math.random()); //setNumber라는 변수가 가리키는 함수의 인자로 변경하려는 새로운 값을 넘겨주면 된다.
+          }
+        }></input>
     </div>
   );
 }
