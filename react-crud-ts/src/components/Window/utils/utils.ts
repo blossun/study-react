6a009ad7ddd1bf656ade73fa729eef4fa61372ac
blossun <dnsngu@gmail.com>
2021-05-02 19:23:06 +0900

    fix: any 타입 지정
    error
    - Parameter 'xxx' implicitly has an 'any' type
    => any 타입으로 명시해줘야하는 것 같다.

--- react-crud-ts/src/components/Window/utils/utils.ts
+++ react-crud-ts/src/components/Window/utils/utils.ts
@@ -1,6 +1,6 @@
 export const BASE_PATH = '/api/autocomplete/window'
 
-function leadingZeros(n: string, digits: number) {
+function leadingZeros(n: any, digits: any) {
   let zero = ''
   n = n.toString()
 
@@ -11,8 +11,8 @@ function leadingZeros(n: string, digits: number) {
 }
 
 export function getTimeStamp() {
-  const d: Date = new Date()
-  const s = leadingZeros(`d.getFullYear()`, 4) + '-' + leadingZeros(`d.getMonth()` + 1, 2) + '-' + leadingZeros(`d.getDate()`, 2) + ' ' + leadingZeros(`d.getHours()`, 2) + ':' + leadingZeros(`d.getMinutes()`, 2) + ':' + leadingZeros(`d.getSeconds()`, 2)
+  const d = new Date()
+  const s = leadingZeros(d.getFullYear(), 4) + '-' + leadingZeros(d.getMonth() + 1, 2) + '-' + leadingZeros(d.getDate(), 2) + ' ' + leadingZeros(d.getHours(), 2) + ':' + leadingZeros(d.getMinutes(), 2) + ':' + leadingZeros(d.getSeconds(), 2)
 
   return s
 }
